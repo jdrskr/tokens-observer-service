80d479dbbc46dcbe26c033a0089fdbff59597d66
jdrskr <jedrzej.skrodzki@gmail.com>
2023-01-12 22:30:59 +0100

    feat: continue swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ApiModule } from './api/api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Token Observer Service')
+    .setDescription('The token observer service API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('public/api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
