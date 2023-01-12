fff42c6eaedcd7aac0d54e1007cc443f3f3fd9bd
jdrskr <jedrzej.skrodzki@gmail.com>
2023-01-12 22:27:50 +0100

    feat: request validator.

--- src/api/token/dto/create-token.dto.ts
+++ src/api/token/dto/create-token.dto.ts
@@ -1,9 +1,25 @@
 import { ChainEnum } from '@domain/index';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export interface CreateTokenDto {
+export class CreateTokenDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   symbol: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.toLowerCase())
   address: string;
+
+  @IsNotEmpty()
   chain: ChainEnum;
+
+  @IsOptional()
+  @IsString()
   updateContinuously?: boolean;
 }
